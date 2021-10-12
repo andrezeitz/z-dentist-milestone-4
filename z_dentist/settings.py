17dce0ed1f718c4500fa0b85be0d7d69283d07dc
andrezeitz <swe_zeitz@hotmail.com>
2021-10-12 17:13:29 +0000

    Small change to the settings.py

--- z_dentist/settings.py
+++ z_dentist/settings.py
@@ -142,4 +142,5 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
