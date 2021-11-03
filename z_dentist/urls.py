61de07671a6197245d6457b1bf88079f7df8ca1e
andrezeitz <swe_zeitz@hotmail.com>
2021-11-03 19:57:19 +0000

    Add logged in user name to navbar top right corner

--- z_dentist/urls.py
+++ z_dentist/urls.py
@@ -1,18 +1,18 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, reverse
 
 from django.views.generic import TemplateView
 from emailer import views
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls, name='admin:login'),
     path('', include('appointment.urls')),
 
     path('', TemplateView.as_view(template_name='index.html'), name='home'),
     path("services/", TemplateView.as_view(template_name='services.html'), name="services"),
     path('contact-us/', TemplateView.as_view(template_name='contact_us.html'), name='contact_us'),
     path('send-form-email/', views.SendFormEmail.as_view(), name='send_email'),
-    
+
     path('accounts/', include('allauth.urls')),
 ]
